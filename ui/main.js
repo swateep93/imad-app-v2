58ce8e53f155d21196abab7d27eb619b6e4b4db4
swateep93 <swatee.parida93@gmail.com>
2017-03-24 00:30:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'New value' ;
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft - marginLeft + 10;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
     
 }
