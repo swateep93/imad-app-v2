8309fb633f80d6d19ac57cbe18b9cda76616980d
swateep93 <swatee.parida93@gmail.com>
2017-03-23 23:31:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,11 @@ console.log('Loaded!');
 var element = document.getElementById('main-text'
 );
 element.innerHTML = 'New value' ;
+//move the image
+var img = document.getElementById('img');
+img.onclick = function () {
+  img.style.marginleft = '100px'   ;
+  
+};
+
+
