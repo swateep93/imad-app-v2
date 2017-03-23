e6f6aac5b3e875bd2a306dcf70dd9f513bf0f67b
swateep93 <swatee.parida93@gmail.com>
2017-03-24 00:25:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,14 @@ var element = document.getElementById('main-text'
 element.innerHTML = 'New value' ;
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft - marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    
+}
 img.onclick = function () {
-  img.style.marginleft = '100px'   ;
+    var interval - setInterval(moveRight, 100);
   
 };
 
